e88047861798d5536587ee6a1fcb2998ec66b46c
priti-singh-ai <pritiex6@gmail.com>
2026-06-22 23:12:42 +0530

    Create Valid Parentheses.py

---
+++ arrays/Valid Parentheses.py
@@ -0,0 +1,49 @@
+"""
+Problem: Valid Parentheses
+You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Return true if s is a valid string, and false otherwise.
+Example:
+Input: s = "[]"
+
+Output: true
+
+Difficulty: Easy
+Pattern: HashMap
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+
+
+
+        
+
+
+
+
+
+
+
+
+
